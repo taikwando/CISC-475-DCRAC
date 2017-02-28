a9ce7143f1024a36184ed9a959606f4e09bdb892
Jessica Chen <JC@wifi-roaming-128-4-159-74.host.udel.edu>
2017-02-28 14:50:42 -0500

    xcode project

---
+++ 475Project4/475Project4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  475Project4
+//
+//  Created by Jessica Chen on 2/28/17.
+//  Copyright © 2017 group4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
